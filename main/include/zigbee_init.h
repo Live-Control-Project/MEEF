3131b17800523613a63b8c8a443b11422bdb9c7f
Live Control <info@meef.ru>
2023-11-08 10:03:50 +0300

    07_11_23

--- main/include/zigbee_init.h
+++ main/include/zigbee_init.h
@@ -4,6 +4,11 @@
 #define MAX_CHILDREN 10                 /* the max amount of connected devices */
 #define INSTALLCODE_POLICY_ENABLE false /* enable the install code policy for security */
 #define SENSOR_ENDPOINT 1
+#define SENSOR_ENDPOINT2 2
+#define SENSOR_ENDPOINT3 3
+#define SENSOR_ENDPOINT4 4
+#define SENSOR_ENDPOINT5 5
+
 #define CO2_CUSTOM_CLUSTER 0xFFF2                                        /* Custom cluster used because standart cluster not working*/
 #define ESP_ZB_PRIMARY_CHANNEL_MASK ESP_ZB_TRANSCEIVER_ALL_CHANNELS_MASK /* Zigbee primary channel mask use in the example */
 #define OTA_UPGRADE_MANUFACTURER 0x1001                                  /* The attribute indicates the file version of the downloaded image on the device*/
@@ -11,8 +16,8 @@
 #define OTA_UPGRADE_FILE_VERSION 0x01010101                              /* The attribute indicates the file version of the running firmware image on the device */
 #define OTA_UPGRADE_HW_VERSION 0x0101                                    /* The parameter indicates the version of hardware */
 #define OTA_UPGRADE_MAX_DATA_SIZE 64                                     /* The parameter indicates the maximum data size of query block image */
-#define MANUFACTURER_NAME "Lmahmutov"
-#define MODEL_NAME "Air Sensor 1.0"
+#define MANUFACTURER_NAME "MEEF"
+#define MODEL_NAME "DEV 1.0"
 #define FIRMWARE_VERSION "ver-0.1"
 
 #define ESP_ZB_ZR_CONFIG()                                \
@@ -35,3 +40,4 @@
     }
 
 void zigbee_init(void);
+void app_main(void);
--- main/zigbee_init.c
+++ main/zigbee_init.c
@@ -7,21 +7,98 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "zcl/esp_zigbee_zcl_common.h"
+#include "sensair_s8.h"
+#include "driver/i2c.h"
+#include "bmx280.h"
+#include "ssd1306.h"
+#include "zigbee_logo.h"
+#include "zigbee_connected.h"
+#include "zigbee_disconnected.h"
+#include "zigbee_image.h"
+#include "iot_button.h"
 #include <time.h>
 #include <sys/time.h>
+#include "cJSON.h"
 
 /*------ Clobal definitions -----------*/
 static char manufacturer[16], model[16], firmware_version[16];
-bool time_updated = false, connected = false;
+bool time_updated = false, connected = false, DEMO_MODE = true; /*< DEMO_MDE disable all real sensors and send fake data*/
 int lcd_timeout = 30;
 uint8_t screen_number = 0;
 uint16_t temperature = 0, humidity = 0, pressure = 0, CO2_value = 0;
 float temp = 0, pres = 0, hum = 0;
-static const char *TAG = "ESP_HA_CO2_SENSOR";
+char strftime_buf[64];
+static const char *TAG = "ZIGBEE";
 
 #if !defined CONFIG_ZB_ZCZR
 #error Define ZB_ZCZR in idf.py menuconfig to compile light (Router) source code.
 #endif
+
+static void button_single_click_cb(void *arg, void *usr_data)
+{
+    // ESP_LOGI("Button boot", "Single click, change screen to %d", screen_number);
+}
+
+static void button_long_press_cb(void *arg, void *usr_data)
+{
+    ESP_LOGI("Button boot", "Long press, leave & reset");
+    esp_zb_factory_reset();
+}
+
+void register_button()
+{
+    // create gpio button
+    button_config_t gpio_btn_cfg = {
+        .type = BUTTON_TYPE_GPIO,
+        .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
+        .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
+        .gpio_button_config = {
+            .gpio_num = 9,
+            .active_level = 0,
+        },
+    };
+
+    button_handle_t gpio_btn = iot_button_create(&gpio_btn_cfg);
+    if (NULL == gpio_btn)
+    {
+        ESP_LOGE("Button boot", "Button create failed");
+    }
+
+    iot_button_register_cb(gpio_btn, BUTTON_SINGLE_CLICK, button_single_click_cb, NULL);
+    iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_START, button_long_press_cb, NULL);
+}
+
+/* --------- User task section -----------------*/
+static void get_rtc_time()
+{
+    time_t now;
+    struct tm timeinfo;
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), "%a %H:%M:%S", &timeinfo);
+}
+
+static void demo_task()
+{
+    while (1)
+    {
+        if (connected)
+        {
+            temperature = rand() % (3000 + 1 - 1000);
+            humidity = rand() % (9000 + 1 - 2000);
+            pressure = rand() % (1000 + 1 - 800);
+            CO2_value = rand() % (3000 + 1 - 400);
+            ESP_LOGI("DEMO MODE", "Temp = %d, Humm = %d, Press = %d, CO2_Value = %d", temperature, humidity, pressure, CO2_value);
+            if (time_updated)
+            {
+                get_rtc_time();
+                ESP_LOGI("DEMO MODE", "The current date/time is: %s", strftime_buf);
+            }
+        }
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+    }
+}
+/*----------------------------------------*/
 
 static void bdb_start_top_level_commissioning_cb(uint8_t mode_mask)
 {
@@ -45,6 +122,26 @@ static void reportAttribute(uint8_t endpoint, uint16_t clusterID, uint16_t attri
     esp_zb_zcl_attr_t *value_r = esp_zb_zcl_get_attribute(endpoint, clusterID, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, attributeID);
     memcpy(value_r->data_p, value, value_length);
     esp_zb_zcl_report_attr_cmd_req(&cmd);
+}
+
+/* Task for update attribute value */
+void update_attribute()
+{
+    while (1)
+    {
+        if (connected)
+        {
+            /* Write new temperature value */
+            esp_zb_zcl_status_t state_tmp = esp_zb_zcl_set_attribute_val(SENSOR_ENDPOINT, ESP_ZB_ZCL_CLUSTER_ID_TEMP_MEASUREMENT, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID, &temperature, false);
+
+            /* Check for error */
+            //    if (state_tmp != ESP_ZB_ZCL_STATUS_SUCCESS)
+            //    {
+            //        ESP_LOGE(TAG, "Setting temperature attribute failed!");
+            //    }
+        }
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
 }
 
 static esp_err_t zb_attribute_handler(const esp_zb_zcl_set_attr_value_message_t *message)
@@ -176,6 +273,124 @@ static void set_zcl_string(char *buffer, char *value)
     memcpy(buffer + 1, value, buffer[0]);
 }
 
+static void esp_zb_task(void *pvParameters)
+{
+    /* initialize Zigbee stack */
+    esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZR_CONFIG();
+    esp_zb_init(&zb_nwk_cfg);
+
+    uint16_t undefined_value;
+    undefined_value = 0x8000;
+    /* basic cluster create with fully customized */
+    set_zcl_string(manufacturer, MANUFACTURER_NAME);
+    set_zcl_string(model, MODEL_NAME);
+    set_zcl_string(firmware_version, FIRMWARE_VERSION);
+    uint8_t dc_power_source;
+    dc_power_source = 4;
+    esp_zb_attribute_list_t *esp_zb_basic_cluster = esp_zb_zcl_attr_list_create(ESP_ZB_ZCL_CLUSTER_ID_BASIC);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_MANUFACTURER_NAME_ID, manufacturer);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_MODEL_IDENTIFIER_ID, model);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_SW_BUILD_ID, firmware_version);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_POWER_SOURCE_ID, &dc_power_source); /**< DC source. */
+
+    /* identify cluster create with fully customized */
+    uint8_t identyfi_id;
+    identyfi_id = 0;
+    esp_zb_attribute_list_t *esp_zb_identify_cluster = esp_zb_zcl_attr_list_create(ESP_ZB_ZCL_CLUSTER_ID_IDENTIFY);
+    esp_zb_identify_cluster_add_attr(esp_zb_identify_cluster, ESP_ZB_ZCL_CMD_IDENTIFY_IDENTIFY_ID, &identyfi_id);
+
+    /* Time cluster */
+    esp_zb_attribute_list_t *esp_zb_server_time_cluster = esp_zb_zcl_attr_list_create(ESP_ZB_ZCL_CLUSTER_ID_TIME);
+
+    /** Create ota client cluster with attributes.
+     *  Manufacturer code, image type and file version should match with configured values for server.
+     *  If the client values do not match with configured values then it shall discard the command and
+     *  no further processing shall continue.
+     */
+    esp_zb_ota_cluster_cfg_t ota_cluster_cfg = {
+        .ota_upgrade_downloaded_file_ver = OTA_UPGRADE_FILE_VERSION,
+        .ota_upgrade_manufacturer = OTA_UPGRADE_MANUFACTURER,
+        .ota_upgrade_image_type = OTA_UPGRADE_IMAGE_TYPE,
+    };
+    esp_zb_attribute_list_t *esp_zb_ota_client_cluster = esp_zb_ota_cluster_create(&ota_cluster_cfg);
+    /** add client parameters to ota client cluster */
+    esp_zb_ota_upgrade_client_parameter_t ota_client_parameter_config = {
+        .query_timer = ESP_ZB_ZCL_OTA_UPGRADE_QUERY_TIMER_COUNT_DEF, /* time interval for query next image request command */
+        .hardware_version = OTA_UPGRADE_HW_VERSION,                  /* version of hardware */
+        .max_data_size = OTA_UPGRADE_MAX_DATA_SIZE,                  /* maximum data size of query block image */
+    };
+    void *ota_client_parameters = esp_zb_ota_client_parameter(&ota_client_parameter_config);
+    esp_zb_ota_cluster_add_attr(esp_zb_ota_client_cluster, ESP_ZB_ZCL_ATTR_OTA_UPGRADE_CLIENT_PARAMETER_ID, ota_client_parameters);
+
+    /* Create full cluster list enabled on device */
+    esp_zb_cluster_list_t *esp_zb_cluster_list = esp_zb_zcl_cluster_list_create();
+    esp_zb_cluster_list_add_basic_cluster(esp_zb_cluster_list, esp_zb_basic_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_identify_cluster(esp_zb_cluster_list, esp_zb_identify_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_time_cluster(esp_zb_cluster_list, esp_zb_server_time_cluster, ESP_ZB_ZCL_CLUSTER_CLIENT_ROLE);
+    esp_zb_cluster_list_add_ota_cluster(esp_zb_cluster_list, esp_zb_ota_client_cluster, ESP_ZB_ZCL_CLUSTER_CLIENT_ROLE);
+
+    esp_zb_ep_list_t *esp_zb_ep_list = esp_zb_ep_list_create();
+
+    cJSON *root;
+    root = cJSON_CreateObject();
+
+    cJSON_AddStringToObject(root, "cluster", "temperature");
+    cJSON_AddNumberToObject(root, "cluster_list", 0);
+    cJSON_AddNumberToObject(root, "EP", 1);
+
+    cJSON *obj = cJSON_GetObjectItem(root, "cluster");
+    if (obj)
+    {
+        char *cluster = cJSON_GetObjectItem(root, "cluster")->valuestring;
+
+        ESP_LOGI(TAG, "cluster=%s", cJSON_GetObjectItem(root, "cluster")->valuestring);
+        if (strcmp(cluster, "temperature") == 0) // Changed from single quotes to double quotes
+        {
+            ESP_LOGI(TAG, "cluster=%s", cJSON_GetObjectItem(root, "cluster")->valuestring);
+            // 3 - Создаем кластеры
+            esp_zb_attribute_list_t *esp_zb_temperature_meas_cluster = esp_zb_zcl_attr_list_create(ESP_ZB_ZCL_CLUSTER_ID_TEMP_MEASUREMENT);
+            esp_zb_temperature_meas_cluster_add_attr(esp_zb_temperature_meas_cluster, ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID, &undefined_value);
+            esp_zb_temperature_meas_cluster_add_attr(esp_zb_temperature_meas_cluster, ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_MIN_VALUE_ID, &undefined_value);
+            esp_zb_temperature_meas_cluster_add_attr(esp_zb_temperature_meas_cluster, ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_MAX_VALUE_ID, &undefined_value);
+
+            // 2 - Запихиваем в cluster_list
+            int cluster_list = cJSON_GetObjectItem(root, "cluster_list")->valueint;
+            if (cluster_list == 0)
+            {
+                esp_zb_cluster_list_add_temperature_meas_cluster(esp_zb_cluster_list, esp_zb_temperature_meas_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+            }
+            // 1 - Создаем EP
+            int EP = cJSON_GetObjectItem(root, "EP")->valueint;
+            esp_zb_ep_list_add_ep(esp_zb_ep_list, esp_zb_cluster_list, EP, ESP_ZB_AF_HA_PROFILE_ID, ESP_ZB_HA_SIMPLE_SENSOR_DEVICE_ID);
+        }
+    }
+    cJSON_Delete(root);
+
+    esp_zb_device_register(esp_zb_ep_list);
+    esp_zb_core_action_handler_register(zb_action_handler);
+    esp_zb_set_primary_network_channel_set(ESP_ZB_PRIMARY_CHANNEL_MASK);
+    ESP_ERROR_CHECK(esp_zb_start(true));
+    esp_zb_main_loop_iteration();
+}
+
 void zigbee_init(void)
 {
+    register_button();
+
+    // sensair_get_info();
+    esp_zb_platform_config_t config = {
+        .radio_config = ESP_ZB_DEFAULT_RADIO_CONFIG(),
+        .host_config = ESP_ZB_DEFAULT_HOST_CONFIG(),
+    };
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_zb_platform_config(&config));
+    if (!DEMO_MODE)
+    {
+    }
+    else
+    {
+        xTaskCreate(demo_task, "demo_task", 4096, NULL, 1, NULL);
+    }
+    xTaskCreate(update_attribute, "Update_attribute_value", 4096, NULL, 5, NULL);
+    xTaskCreate(esp_zb_task, "Zigbee_main", 4096, NULL, 6, NULL);
 }
